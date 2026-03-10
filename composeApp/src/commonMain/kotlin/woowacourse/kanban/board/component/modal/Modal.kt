a77c2ae3838fa92bd40cce1f8af9eef1dbb003a6
joon0447 <joon0447@naver.com>
2026-03-10 18:56:42 +0900

    feat(modal): Card 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/modal/Modal.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/modal/Modal.kt
@@ -1,12 +1,17 @@
 package woowacourse.kanban.board.component.modal
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -18,15 +23,23 @@ private fun ModalPreview() {
 
 @Composable
 fun Modal() {
-    Column(modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Header()
-        HorizontalDivider()
-        TextInputSection("제목", "태스크 제목을 입력하세요")
-        TextInputSection("제목", "태스크 제목을 입력하세요", Modifier.height(200.dp))
-        TextInputSection("제목", "태스크 제목을 입력하세요")
-        ButtonSection("상태", listOf("1", "2", "3"))
-        ButtonSection("상태", listOf("1", "2"))
-        Footer()
+    Card(
+        modifier = Modifier
+            .padding(50.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        )
+    ) {
+        Column(modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Header()
+            HorizontalDivider()
+            TextInputSection("제목", "태스크 제목을 입력하세요")
+            TextInputSection("제목", "태스크 제목을 입력하세요", Modifier.height(200.dp))
+            TextInputSection("제목", "태스크 제목을 입력하세요")
+            ButtonSection("상태", listOf("1", "2", "3"))
+            ButtonSection("상태", listOf("1", "2"))
+            Footer()
+        }
     }
 }
