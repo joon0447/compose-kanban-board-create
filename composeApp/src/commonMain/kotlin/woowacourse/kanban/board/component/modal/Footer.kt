7f93af995bb4a577e39ab53b12c6c067402bbaa6
joon0447 <joon0447@naver.com>
2026-03-10 15:53:27 +0900

    feat(footer): 모달 푸터 구현

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/modal/Footer.kt
@@ -0,0 +1,64 @@
+package woowacourse.kanban.board.component.modal
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+private fun FooterPreview() {
+    Footer()
+}
+
+@Composable
+fun Footer() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.End
+    ) {
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .clip(RoundedCornerShape(10.dp)),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color(0xFF364153)
+            )
+        ) {
+            Text(
+                text = "취소",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal
+            )
+        }
+        Spacer(modifier = Modifier.width(12.dp))
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .clip(RoundedCornerShape(10.dp)),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF4F39F6)
+            )
+        ) {
+            Text(
+                text = "생성",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal
+            )
+        }
+    }
+}
