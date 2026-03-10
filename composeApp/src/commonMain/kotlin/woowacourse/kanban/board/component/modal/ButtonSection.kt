ab1b0fe9b78395cba90a28048369aa07a1590539
joon0447 <joon0447@naver.com>
2026-03-10 17:45:08 +0900

    feat(buttonsection): 버튼 섹션 구현

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/modal/ButtonSection.kt
@@ -0,0 +1,80 @@
+package woowacourse.kanban.board.component.modal
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.w3c.dom.Text
+
+@Composable
+@Preview(showBackground = true)
+private fun ButtonSectionPreview() {
+    ButtonSection("제목", listOf("1", "2"))
+}
+
+@Composable
+fun ButtonSection(
+    label: String,
+    items: List<String>,
+) {
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        Text(
+            text = label,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = Color(0xFF364153),
+        )
+        Row(
+            modifier = Modifier.height(52.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+
+        ) {
+            for (item in items) {
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .border(1.dp, color = Color(0xFFE5E7EB), shape = RoundedCornerShape(10.dp))
+                        .padding(horizontal = 50.dp, vertical = 14.dp),
+                ) {
+                    Text(
+                        text = item,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth(),
+                        fontSize = 16.sp,
+                        color = Color(0xFF364153),
+                        fontWeight = FontWeight.Normal
+                    )
+                }
+            }
+        }
+    }
+}
