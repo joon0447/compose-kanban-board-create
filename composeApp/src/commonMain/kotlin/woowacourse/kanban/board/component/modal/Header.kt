0ff16e67e5d880b1fee143a9e239f8b20afdd49f
joon0447 <joon0447@naver.com>
2026-03-10 15:51:52 +0900

    feat(header): 모달 헤더 구현

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/modal/Header.kt
@@ -0,0 +1,46 @@
+package woowacourse.kanban.board.component.modal
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+fun HeaderPreview() {
+    Header()
+}
+
+@Composable
+fun Header() {
+    Row (
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp)
+    ) {
+        Text(
+            text = "새 태스크 생성",
+            fontSize = 20.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold
+        )
+        Icon(
+            imageVector = Icons.Default.Close,
+            contentDescription = "닫기"
+        )
+    }
+}
