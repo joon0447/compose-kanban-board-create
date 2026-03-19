7589aed92fca8c941b4836773ff086bde3001254
joon0447 <joon0447@naver.com>
2026-03-19 16:02:37 +0900

    test: TaskCardData 진행 상태별 저장 테스트 추가

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/state/BoardStateTest.kt
@@ -0,0 +1,59 @@
+package woowacourse.kanban.board.model.state
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import woowacourse.kanban.board.model.TaskState
+import woowacourse.kanban.board.model.modal.Description
+import woowacourse.kanban.board.model.modal.ProfileState
+import woowacourse.kanban.board.model.modal.Tags
+import woowacourse.kanban.board.model.modal.Title
+import woowacourse.kanban.board.model.taskcard.TaskCardData
+import kotlin.test.Test
+
+class BoardStateTest {
+    private lateinit var boardState: BoardState
+
+    @Before
+    fun setUp() {
+        boardState = BoardState()
+    }
+
+    @Test
+    fun `Todo TaskCardData를 추가하면 todoList에 저장된다`() {
+        val data = TaskCardData(
+            title = Title(value = "업무1"),
+            description = Description(""),
+            tags = Tags(value = "컴포넌트"),
+            task = TaskState.TODO,
+            profile = ProfileState.DINO
+        )
+        boardState.addCard(data)
+        assertThat(boardState.toDoList).contains(data)
+    }
+
+    @Test
+    fun `Progress TaskCardData를 추가하면 progressList에 저장된다`() {
+        val data = TaskCardData(
+            title = Title(value = "업무1"),
+            description = Description(""),
+            tags = Tags(value = "컴포넌트"),
+            task = TaskState.PROGRESS,
+            profile = ProfileState.DINO
+        )
+        boardState.addCard(data)
+        assertThat(boardState.progressList).contains(data)
+    }
+
+    @Test
+    fun `Done TaskCardData를 추가하면 doneList에 저장된다`() {
+        val data = TaskCardData(
+            title = Title(value = "업무1"),
+            description = Description(""),
+            tags = Tags(value = "컴포넌트"),
+            task = TaskState.DONE,
+            profile = ProfileState.DINO
+        )
+        boardState.addCard(data)
+        assertThat(boardState.doneList).contains(data)
+    }
+}
