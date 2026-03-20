d6e09c64617e53532d4e2333cf9b9915a6994137
joon0447 <joon0447@naver.com>
2026-03-20 20:35:29 +0900

    feat: TaskColumn 여백 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/board/TaskColumnSection.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/board/TaskColumnSection.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -60,6 +61,9 @@ fun TaskColumnSection(
             taskState = TaskState.DONE,
             tasks = doneTasks,
             modifier = Modifier.weight(1f),
+        )
+        Spacer(
+            modifier = Modifier.weight(.7f)
         )
     }
 }
