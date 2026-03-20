613c80dd8410617ace99e23b2f069ec5333c0200
joon0447 <joon0447@naver.com>
2026-03-20 21:14:29 +0900

    test: 스낵바 출력 테스트 추가

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/component/board/BoardTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/component/board/BoardTest.kt
@@ -4,12 +4,15 @@ import androidx.compose.ui.semantics.SemanticsActions
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.isEditable
+import androidx.compose.ui.test.isRoot
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performSemanticsAction
 import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.printToLog
 import androidx.compose.ui.test.runComposeUiTest
 import kotlin.test.Test
 import woowacourse.kanban.board.component.ComponentText
@@ -58,5 +61,17 @@ class BoardTest {
         onAllNodes(isEditable())[0].performTextInput("테스트에용")
         onAllNodes(hasText(ComponentText.CREATE_BUTTON))[0].performSemanticsAction(SemanticsActions.OnClick)
         onNodeWithText("테스트에용").assertExists()
+    }
+
+    @Test
+    fun `태스크 카드를 생성하면 스낵바가 출력된다`() = runComposeUiTest {
+        setContent {
+            Board()
+        }
+        onNodeWithText(ComponentText.BOARD_TASK_CREATE_BUTTON).performClick()
+        onAllNodes(isEditable())[0].performClick()
+        onAllNodes(isEditable())[0].performTextInput("테스트에용")
+        onAllNodes(hasText(ComponentText.CREATE_BUTTON))[0].performSemanticsAction(SemanticsActions.OnClick)
+        onNodeWithText(ComponentText.BOARD_TASK_CREATE_SNACKBAR).isDisplayed()
     }
 }
